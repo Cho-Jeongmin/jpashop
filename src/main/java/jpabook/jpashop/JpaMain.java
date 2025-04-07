d99ac868de4b0ee7a9a6a06cf7079b14d5c126fa
Jeongmin Cho <jojm0829@gmail.com>
2025-04-07 20:15:59 +1000

    11.1 소개 - Criteria

--- src/main/java/jpabook/jpashop/JpaMain.java
+++ src/main/java/jpabook/jpashop/JpaMain.java
@@ -4,6 +4,10 @@ import jakarta.persistence.EntityManager;
 import jakarta.persistence.EntityManagerFactory;
 import jakarta.persistence.EntityTransaction;
 import jakarta.persistence.Persistence;
+import jakarta.persistence.criteria.CriteriaBuilder;
+import jakarta.persistence.criteria.CriteriaQuery;
+import jakarta.persistence.criteria.Root;
+import java.util.List;
 import jpabook.jpashop.domain.Book;
 import jpabook.jpashop.domain.Item;
 import jpabook.jpashop.domain.Member;
@@ -21,14 +25,19 @@ public class JpaMain {
     tx.begin();
 
     try {
-      Book book = new Book();
-      book.setName("JPA");
-      book.setAuthor("김영한");
+      CriteriaBuilder cb = em.getCriteriaBuilder();
+      CriteriaQuery<Member> query = cb.createQuery(Member.class);
 
-      em.persist(book);
+      Root<Member> m = query.from(Member.class);
+
+      CriteriaQuery<Member> cq = query.select(m).where(cb.equal(m.get("username"), "kim"));
+      List<Member> resultList = em.createQuery(cq).getResultList();
+
+      // 실무에서는 Criteria 말고 QueryDSL 사용
 
       tx.commit();
     } catch (Exception e) {
+      e.printStackTrace();
       tx.rollback();
     } finally {
       em.close();
